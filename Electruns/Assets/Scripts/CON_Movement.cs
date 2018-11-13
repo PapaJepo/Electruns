4ecba14a4969d02695d4aa42596c005dd0a911e6
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2018-11-13 15:22:50 +0000

    First Commit

---
+++ Electruns/Assets/Scripts/CON_Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CON_Movement : MonoBehaviour {
+
+    private float speed = 3f;
+    private float CharSwap = -1;
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        bool Swap = Input.GetKey(KeyCode.Space);
+        if(Swap)
+        {
+            CharSwap = CharSwap * -1;
+
+        }
+
+        if (CharSwap < 0)
+        {
+            var x = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+            var z = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+            transform.Translate(z, 0, 0);
+            transform.Translate(0, x, 0);
+        }
+    }
+}
---
+++ Electruns/Assets/Scripts/Door.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    private Renderer DoorSprite;
+    private float CONcounter, INcounter;
+	void Start ()
+    {
+        DoorSprite = GetComponent<Renderer>();
+	}
+	
+	
+	void Update ()
+    {
+		if((CONcounter > 0) &&(INcounter > 0))
+        {
+            DoorSprite.enabled = false;
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Conductor")
+        {
+            Debug.Log("Enter");
+            CONcounter++;
+        }
+        if (collision.tag == "Insulator")
+        {
+            Debug.Log("Enter");
+            INcounter++;
+        }
+    }
+  
+}
